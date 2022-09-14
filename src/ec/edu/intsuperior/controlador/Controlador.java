bdb980e48c31a0e8ca50f80f01c92c2e64d6bae1
casae <casae@DESKTOP-CPOTUVF>
2022-09-13 21:35:28 -0500

    Este proyecto es la v1.1

---
+++ src/ec/edu/intsuperior/controlador/Controlador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+
+import ec.edu.intsuperior.vista.JpAyuda;
+import ec.edu.intsuperior.vista.Login;
+
+/**
+ *
+ * @author casae
+ */
+public class Controlador {
+    
+    
+    
+    Login login =new Login );
+    JpAyuda jpAyuda = new JpAyuda(this
+    
+    
+}
---
+++ src/ec/edu/intsuperior/controlador/conexion.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+
+import java.net.ConnectException;
+
+/**
+ *
+ * @author casae
+ */
+public class conexion {
+    private static Conneccion con;
+    private static String url="jbdc:mysql://localhost:3306/hosteria";
+    private static 
+
+            
+            
+    
+    }
+    
+
